ab08a68f26d609379f99e5d4d82152d4f18a3f9e
Ben Cohen <bj.cohen19@gmail.com>
2012-09-13 08:10:59 -0400

    PA#: buggy broken ugly parsec parser

--- src/ParserParsec.hs
+++ src/ParserParsec.hs
@@ -16,6 +16,10 @@ import SCUtil
 import Type
 import Parser.PrettyPrint (pp)
 
+import Data.Char ( toLower )
+import Data.List ( sort )
+
+
 type ST = Map.Map Symbol String
 type ParseMonad = WriterT ST (Supply Int)
 
@@ -23,8 +27,10 @@ instance MonadSupply s m => MonadSupply s (ParsecT s' u m) where
   supply = lift supply
   
 instance MonadWriter w m => MonadWriter w (ParsecT s u m) where
-  tell = lift . tell
-  --TODO: finish this instance declaration
+  tell = lift . tell                  -- tell :: w -> m ()
+  listen = mkPT . runParsecT . listen -- listen :: m a -> m (a, w)
+  pass = mkPT . runParsecT . pass     -- pass :: m (a, w -> w) -> m a
+  --TODO: are these right?
 
 addString :: String -> ParseMonad Symbol
 addString val =
@@ -40,12 +46,10 @@ addStringP val =
      tell $ Map.singleton sym val
      return $ sym
               
--- type ParseMonad = Supply Int
-
 runCoolParser :: CoolParser a -> String -> SourceName -> (Either ParseError a, ST)
 runCoolParser p input fname =
   evalSupply (runWriterT $ runParserT p fname fname input) [0..]
-  
+
 type UserData = String
 
 type CoolParser = ParsecT String UserData ParseMonad
@@ -77,16 +81,19 @@ feature = try (do
   init <- option (mkE NoExpr) (do { reservedOp "<-"; expr })
   return $ Attr{attrName=newSymbol name,attrType=newSymbol val_type,
                 attrInit=init})
-  <|> do
+  <|> try (do
   name <- obj_id
   args <- parens (sepBy formal comma)
   colon
   ret_type <- type_id
   body <- braces expr
   return $ Method{methodName=newSymbol name,methodFormals=args,
-                  methodReturnType=newSymbol ret_type,methodExpr=body}
+                  methodReturnType=newSymbol ret_type,methodExpr=body})
   <?> "feature"
 
+-- TODO: error handling - class, feature, let binding, expr inside block          
+-- these all end with semicolons except the let bindings..
+          
 formal :: CoolParser Formal
 formal = do
   fname <- obj_id
@@ -96,40 +103,42 @@ formal = do
   <?> "formal"
 
 expr :: CoolParser Expression
-expr = 
-  try (buildExpressionParser operatorTable term)
-  <|> do
-  { name <- obj_id; reservedOp "<-"; body <- expr;
-    return $ mkE Assign{assignName=newSymbol name,assignExpr=body} }
-  -- <|> do
-  -- { e <- option (mkE Object{objectName=newSymbol "self"}) (do e <- expr; dot; return e);
-  --   name <- obj_id; args <- parens (sepBy expr comma);
-  --   return $ mkE Dispatch{dispatchExpr=e,dispatchName=newSymbol name,dispatchActual=args}}
-  -- <|> do
-  -- { e <- expr; reservedOp "@"; typ <- type_id; dot; name <- obj_id;
-  --   args <- parens (sepBy expr comma);
-  --   return $ mkE StaticDispatch{staticDispatchExpr=e,staticDispatchType=newSymbol typ,
-  --                               staticDispatchName=newSymbol name,staticDispatchActual=args}}
-  <|> do
-  { reserved "if"; e1 <- expr;
-    reserved "then"; e2 <- expr;
-    reserved "else"; e3 <- expr;
-    reserved "fi";
-    return $ mkE Cond{condPred=e1,condThen=e2,condExpression=e3}}
-  <|> do
-  { reserved "while"; e1 <- expr;
-    reserved "loop"; e2 <- expr;
-    reserved "pool";
-    return $ mkE Loop{loopPred=e1,loopBody=e2}}
+expr =
+  do e <- expr' 
+     exprrest e
+  <|> expr'
+  <?> "expr"
+
+expr' :: CoolParser Expression
+expr' = 
+-- try (buildExpressionParser operatorTable term)
+  try (parens expr)
+  <|> do { reservedOp "~"; e <- expr; return $ mkE Neg{negE=e}}
+  <|> do { reservedOp "isvoid"; e <- expr; return $ mkE IsVoid{isVoidE=e}}
+  <|> do { reservedOp "not"; e <- expr; return $ mkE Comp{compE=e}}
+  <|> try (do
+  { name <- obj_id; asnarr; body <- expr;
+    return $ mkE Assign{assignName=newSymbol name,assignExpr=body}})
+  <|> try (do
+  { name <- obj_id;
+    args <- parens (sepBy expr comma);
+    return $ mkE Dispatch{dispatchExpr=selfobj,dispatchName=newSymbol name,dispatchActual=args}})
+  <|> try term
+  <|> try (do
+  { reserved "if"; e1 <- expr; reserved "then"; e2 <- expr;
+    reserved "else"; e3 <- expr; reserved "fi";
+    return $ mkE Cond{condPred=e1,condThen=e2,condExpression=e3}})
+  <|> try (do
+  { reserved "while"; e1 <- expr; reserved "loop"; e2 <- expr;
+    reserved "pool"; return $ mkE Loop{loopPred=e1,loopBody=e2}})
   <|> do
   { e <- braces (endBy expr semi);
     return $ mkE Block{blockBody=e}}
   <|> do
   { reserved "let";
-    is <- many1 ( do id <- obj_id; colon;
-                     typ <- type_id;
-                     e <- option (mkE NoExpr) (do reservedOp "<-"; e <- expr; return e);
-                     return (id, typ, e) );
+    is <- sepBy1 ( do id <- obj_id; colon; typ <- type_id;                     
+                      e <- option (mkE NoExpr) (do asnarr; e <- expr; return e);
+                      return (id, typ, e) ) comma;
     reserved "in";
     e <- expr;
     return $ mkLetBody is e }
@@ -141,8 +150,54 @@ expr =
     return $ mkE TypCase{typCaseExpr=e,typCaseCases=l}}
   <|> do
   { reserved "new"; typ <- type_id; return $ mkE New{newType=newSymbol typ}}
-  <?> "expr"
+  <?> "expr'"
 
+exprrest :: Expression -> CoolParser Expression
+exprrest e = 
+  (try
+  (do { dot; name <- obj_id; args <- parens (sepBy expr comma);
+        return $ mkE Dispatch{dispatchExpr=e,dispatchName=newSymbol name,
+                              dispatchActual=args}}
+   <|>
+   do { reservedOp "@"; typ <- type_id; dot; name <- obj_id;
+        args <- parens (sepBy expr comma);
+        return $ mkE StaticDispatch{staticDispatchExpr=e,staticDispatchType=newSymbol typ,
+                                    staticDispatchName=newSymbol name,
+                                    staticDispatchActual=args}}
+   <|> exprrestchainer e mulop
+   <|> exprrestchainer e addop
+   <|> exprrestchainer e cmpop)
+   >>= \e' -> exprrest e')
+  <|> return e
+  <?> "exprrest"
+
+mulop :: CoolParser (Expression -> Expression -> Expression)
+mulop =
+  do { reservedOp "*"; return (\e1 e2 -> mkE Mul{mulE1=e1,mulE2=e2}) }
+  <|> do { reservedOp "/"; return (\e1 e2 -> mkE Divide{divideE1=e1,divideE2=e2}) }
+
+addop :: CoolParser (Expression -> Expression -> Expression)
+addop =
+  do { reservedOp "+"; return (\e1 e2 -> mkE Plus{plusE1=e1,plusE2=e2}) }
+  <|> do { reservedOp "-"; return (\e1 e2 -> mkE Sub{subE1=e1,subE2=e2}) }
+
+cmpop :: CoolParser (Expression -> Expression -> Expression)
+cmpop =
+  do { reservedOp "<="; return (\e1 e2 -> mkE Le{leE1=e1,leE2=e2}) }
+  <|> do { reservedOp "<"; return (\e1 e2 -> mkE Lt{ltE1=e1,ltE2=e2}) }
+  <|> do { reservedOp "="; return (\e1 e2 -> mkE Eq{eqE1=e1,eqE2=e2}) }
+  
+exprrestchainer :: Expression -> CoolParser (Expression -> Expression -> Expression)
+                   -> CoolParser Expression
+exprrestchainer x op  =
+  do { f <- op; y <- expr; rest (f x y) op }
+    where
+      rest x' op' = exprrestchainer x' op' <|> return x'
+
+isSameBinOp :: Expression -> Expression -> Bool
+isSameBinOp (Expression Mul{} _) (Expression Mul{} _) = True
+isSameBinOp _ _ = False
+      
 operatorTable :: [[Operator String UserData ParseMonad Expression]]
 operatorTable =
   [ [prefix "~" (\e -> mkE $ Neg{negE=e})],
@@ -160,11 +215,10 @@ binary  name fun assoc = Infix (do{ reservedOp name; return fun }) assoc
 prefix  name fun       = Prefix (do{ reservedOp name; return fun })
 
 term :: CoolParser Expression
-term = parens expr
-       <|> do { name <- obj_id; return $ mkE Object{objectName=newSymbol name}}
+term =     do { b <- boolLit; return $ mkE BoolConst{boolConstVal=b}}
        <|> do { s <- intLit; sy <- addStringP s; return $ mkE IntConst{intConstToken=sy}}
        <|> do { s <- stringLit; sy <- addStringP s; return $ mkE StringConst{stringConstToken=sy}}
-       <|> do { b <- boolLit; return $ mkE BoolConst{boolConstVal=b}}
+       <|> do { name <- obj_id; return $ mkE Object{objectName=newSymbol name}}
        <?> "term"
        
 -- TODO: error handling       
@@ -188,8 +242,8 @@ coolDef = T.LanguageDef {
   nestedComments = True,
   identStart = letter <|> char '_',
   identLetter = alphaNum <|> char '_',
-  opStart = oneOf "=+-*/%&^|<>!~@",
-  opLetter = oneOf "->=",
+  opStart = oneOf "",
+  opLetter = oneOf "",
   reservedNames = ["class","else","fi","if","in","inherits","isvoid","let","loop","pool",
                    "then","while","case","esac","new","of","not"],
   reservedOpNames = ["<-","+","-","*","/","<","<=","=","=>","~","@"],
@@ -210,19 +264,30 @@ colon :: CoolParser ()
 colon = do _ <- T.colon ctokens; return ()
 dot :: CoolParser ()           
 dot = do _ <- T.dot ctokens; return ()
+asnarr :: CoolParser ()
+asnarr = do _ <- T.symbol ctokens "<-"; return ()
 type_id :: CoolParser String
-type_id =
-  (lexeme ctokens $ try $
-   do c <- upper
+type_id = mkIdentifier type_ident
+obj_id :: CoolParser String
+obj_id = mkIdentifier obj_ident
+mkIdentifier :: CoolParser String -> CoolParser String
+mkIdentifier p = lexeme ctokens $ try $
+  do{ name <- p;
+      if (isReservedName name)
+      then unexpected ("reserved word " ++ show name)
+      else return name }
+type_ident :: CoolParser String
+type_ident =
+  (do c <- upper
       cs <- many (identLetter coolDef)
       return $ c:cs)
   <?> "type_id"
-obj_id :: CoolParser String
-obj_id =
-  (lexeme ctokens $ try $
-   do c <- lower
+obj_ident :: CoolParser String
+obj_ident =
+  (do c <- lower
       cs <- many (identLetter coolDef)
       return $ c:cs)
+      
   <?> "obj_id"
 parens :: CoolParser a -> CoolParser a
 parens = T.parens ctokens
@@ -244,10 +309,12 @@ false = do char 'f'
 stringLit :: CoolParser String
 stringLit = T.stringLiteral ctokens
 intLit :: CoolParser String
-intLit = many1 digit
+intLit = lexeme ctokens $ many1 digit
 boolLit :: CoolParser Bool
-boolLit = try (true >> return True)
-          <|> try (false >> return False)
+boolLit = (lexeme ctokens $ try $
+  (true >> return True)
+  <|> (false >> return False))
+  <?> "boolLit"
 
 mkE :: ExpressionBody -> Expression
 mkE e = Expression e NoType
@@ -256,3 +323,34 @@ mkLetBody :: [(String, String, Expression)] -> Expression -> Expression
 mkLetBody [] ef = ef
 mkLetBody ((id,typ,init):las) ef =
   mkE Let{letId=newSymbol id,letType=newSymbol typ,letInit=init,letBody=mkLetBody las ef}
+  
+selfobj :: Expression
+selfobj = mkE Object{objectName=newSymbol "self"};
+
+-- Copied from Parsec.Token
+
+languageDef = coolDef
+
+isReservedName :: String -> Bool
+isReservedName name
+  = isReserved theReservedNames caseName
+    where
+      caseName      | caseSensitive languageDef  = name
+                    | otherwise               = map toLower name
+
+
+isReserved names name
+  = scan names
+    where
+      scan []       = False
+      scan (r:rs)   = case (compare r name) of
+        LT  -> scan rs
+        EQ  -> True
+        GT  -> False
+
+theReservedNames
+  | caseSensitive languageDef  = sort reserved
+  | otherwise                  = sort . map (map toLower) $ reserved
+    where
+      reserved = reservedNames languageDef
+
